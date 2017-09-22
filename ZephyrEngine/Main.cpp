a537928e373ca01f1785a10af6b453931ea65906
Aify <hlo1453@gmail.com>
2017-09-21 23:42:12 -0700

    set up worker threads, and added code that's been commented out for the purpose of speeding up the process later

--- ZephyrEngine/Main.cpp
+++ ZephyrEngine/Main.cpp
@@ -1,19 +1,56 @@
 #include "Main.h"
+#include "ctpl_stl.h"
+
+// insert game object list here
+// List<GameObject> currObjects;
 
 int main(int argc, char *argv[]) { 
-	// entry point
+	// Create worker thread pool
+	ctpl::thread_pool p(16); // number of threads in pool
+	// for p.push usage, see the ctpl_stl.h header file
+
+
 	// create ONE message bus that goes to ALL the systems
 	MessageBus* mbus = new MessageBus();
 
-	// ex: creating a basic system
+	// create IO system
+
+	// Load Scene
+
+	// while 1 & game interval is up
+	/*
+	
+		for (GameObject obj : currObjects) {
+			for (Component c : obj.components) {
+				p.push(c.earlyUpdate());
+			}
+		}
+
+		// wait for all threads to complete
+	
+		for (GameObject obj : currObjects) {
+			for (Component c : obj.components) {
+				p.push(c.update());		
+			}
+		}
+
+		// wait for all threads to complete
+	
+		for (GameObject obj : currObjects) {
+			for (Component c : obj.components) {
+				p.push(c.lateUpdate());		
+			}
+		}
+
+	*/
+	
+	
+
 	/*
 	
 	SomeSystem* sysOne = new SomeSystem(mbus); // note that SomeSystem must extend from System. Pass in the message bus we created so that they're all using the same bus
 	mbus->someSystem = sysOne;	// add the system to the bus so that the bus has a reference to it
 	
-	// thread the system
-	// start the system
-	// repeat for each required system
 	*/
 
 
