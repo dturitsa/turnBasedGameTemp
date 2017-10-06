19ac62429a9d52f9d7b934f10607c80b125e4230
Matthew Wong <matthewwong3.14@gmail.com>
2017-10-06 14:44:46 -0700

    initial window using SDL/OpenGL

--- ZephyrEngine/RenderSystem.cpp
+++ ZephyrEngine/RenderSystem.cpp
@@ -1,8 +1,49 @@
 #include "RenderSystem.h"
-#include <SDL.h>
 
 RenderSystem::RenderSystem(MessageBus* mbus) : System (mbus) {
+	//Initialize SDL
 	SDL_Init(SDL_INIT_EVERYTHING);
+
+	//Setup SDL and GL
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+	//Setup window and context
+	SDL_Window *window = SDL_CreateWindow("SDL OpenGL", XSTART, YSTART, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	glewExperimental = GL_TRUE;
+
+	if (GLEW_OK != glewInit()) {
+	//std:cout << "Failed to init GLEW" << std::endl;
+		return;
+	}
+
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	SDL_Event windowEvent;
+
+	while (true) {
+		if (SDL_PollEvent(&windowEvent)) {
+			if (SDL_QUIT == windowEvent.type) {
+				break;
+			}
+		}
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//draw OpenGL
+
+		SDL_GL_SwapWindow(window);
+	}
+
+	//Window was closed
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
 
