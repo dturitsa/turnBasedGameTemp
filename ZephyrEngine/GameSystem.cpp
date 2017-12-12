22700fc193549ac5e7f0c295cd8704d5c726a8df
Denis Turitsa <dturitsa@gmail.com>
2017-12-12 14:50:29 -0800

    added michael islands to level_1.txt

--- ZephyrEngine/GameSystem.cpp
+++ ZephyrEngine/GameSystem.cpp
@@ -407,7 +407,7 @@ void GameSystem::handleMessage(Msg *msg) {
 				removeAllGameObjects();
 
 				// then, load new objects
-				addGameObjects("Alpha_Level_1.txt"); // TEMPORARY 
+				addGameObjects("Level_1.txt"); // TEMPORARY 
 				levelLoaded = 2;
 				Msg* m = new Msg(LEVEL_LOADED, "2");
 				msgBus->postMessage(m, this);
